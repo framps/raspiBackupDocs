4a76d069746b35f3397a08d8e297aa3d5c655f5c
rpi-simonz <simonz@mailbox.org>
2025-07-09 09:20:28 +0200

    Add YAML file with all cmdline options and a Python script for it.

---
+++ genoptions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+""" Markdown generator
+
+Print a text to stdout containing raspiBackup's commandline options
+formatted as an HTML anchor and Markdown heading and contents.
+
+Every option has a small table in it with some technical infos.
+
+This is special for the documentation in raspiBackupDoc.
+It can be inserted in file 'usage-and-options.md'.
+
+Requires Python 3.9 or higher and PyYAML.
+"""
+
+from pathlib import Path
+
+import yaml
+
+DATA_IN_FILENAME = "de/src/usage-and-options.yaml"
+
+with Path(DATA_IN_FILENAME).open("r", encoding="utf-8") as yamlfile:
+    params = yaml.load(yamlfile, Loader=yaml.CLoader)
+
+for param in sorted(params):
+    p = params[param]
+    try:
+        print(f'<a name="{p["anchor"]}"></a>')
+        print(f'### {param}: {p.get("description", "")}')
+        print()
+        print("\n".join(p["text"]))
+        print()
+        print("| Optionsname | Standard| Im Installer setzbar | Konfigurationsname |")
+        print("|-------------|---------| :------------------: |--------------------|")
+        print(f"| {param} | {p["default"]} | {p["in_inst"]} | {p["cfg_name"]} |")
+        print()
+        print()
+
+    except KeyError as e:
+        print("KEY_ERROR!", str(e), param)
