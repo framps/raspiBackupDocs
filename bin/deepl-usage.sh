76f8da7859d71bbf0330d0336423eb15fc89c656
rpi-simonz <simonz@mailbox.org>
2025-07-16 20:21:36 +0200

    Using jq if installed to prettify output

--- bin/deepl-usage.sh
+++ bin/deepl-usage.sh
@@ -3,6 +3,21 @@
 
 DEEPL_KEY=$(cat ~/.config/deepl.auth_key)
 
-curl --request GET \
+RESULT=$(curl --silent --request GET \
   --url https://api-free.deepl.com/v2/usage \
-  --header "Authorization: DeepL-Auth-Key ${DEEPL_KEY}"
+  --header "Authorization: DeepL-Auth-Key ${DEEPL_KEY}" )
+
+if command -pv jq >/dev/null; then
+    # jq '.' <(echo "${RESULT}")
+    #   {
+    #     "character_count": 753,
+    #     "character_limit": 500000
+    #   }
+    CHAR_COUNT=$(jq '.character_count' <(echo "${RESULT}"))
+    CHAR_LIMIT=$(jq '.character_limit' <(echo "${RESULT}"))
+    PERCENTAGE=$((CHAR_COUNT*100/CHAR_LIMIT))
+    echo "You already consumed $CHAR_COUNT characters from your monthly limit of $CHAR_LIMIT characters (${PERCENTAGE}%)."
+else
+    echo "${RESULT}"
+fi
+
